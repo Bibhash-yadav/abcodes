560f55617d1018776739eb87ab3fc6035693c754
yadavbibhash304059@gmail.com <yadavbibhash304059@gmail.com>
2023-07-03 15:59:47 +0545

    content

---
+++ output/bibhash/javascript.js
@@ -0,0 +1,7 @@
+
+    hamburger = document.querySelector(".hamburger");
+    hamburger.onclick = function () {
+        navbar = document.querySelector(".nav-list");
+        navbar.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    }
